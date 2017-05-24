e98759ef4c272af272554c9204a010542c95e71c
sjoleary <shannon.j.oleary@gmail.com>
2017-05-24 15:22:54 -0500

    synteny mapping stats

--- lib/ggplot.R
+++ lib/ggplot.R
@@ -1,3 +1,4 @@
+
 # modified ggplot themes
 theme_facet <- theme_classic() +
   theme(
@@ -11,7 +12,7 @@ theme_facet <- theme_classic() +
     panel.border = element_rect(fill = NA, color = "black"), 
     panel.grid.major = element_line(color = "grey85"), 
     panel.grid.minor = element_blank(), 
-    strip.background = element_rect(fill = "grey95", color = "black"), 
+    strip.background = element_rect(fill = "white", color = "white"), 
     strip.text.x = element_text(size = 16),
     strip.text.y = element_text(size = 16))  
 
