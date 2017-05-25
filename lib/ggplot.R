5421856eb0cf4de00360feebe5335311c828d48b
sjoleary <shannon.j.oleary@gmail.com>
2017-05-25 14:10:51 -0500

    synteny mapping

--- lib/ggplot.R
+++ lib/ggplot.R
@@ -2,19 +2,20 @@
 # modified ggplot themes
 theme_facet <- theme_classic() +
   theme(
-    axis.text = element_text(size = 11),
+    axis.text = element_text(size = 11, color = "black"),
     axis.title = element_text(size = 16),
     axis.title.y = element_text(vjust = 1.5),
     
     legend.position = "bottom",
     
     panel.background = element_rect(fill = "white", color = NA), 
-    panel.border = element_rect(fill = NA, color = "black"), 
-    panel.grid.major = element_line(color = "grey85"), 
-    panel.grid.minor = element_blank(), 
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    
     strip.background = element_rect(fill = "white", color = "white"), 
-    strip.text.x = element_text(size = 16),
-    strip.text.y = element_text(size = 16))  
+    strip.text.x = element_text(size = 14),
+    strip.text.y = element_text(size = 14))  
 
 theme_standard <- theme_classic() +
   theme(
